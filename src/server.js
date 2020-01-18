35f2d86248c3ab157772a93dcb2caef0c1c66276
ijaewon <ijaewon480@gmail.com>
2020-01-18 17:45:37 +0900

    Create seeFullPost, editUser, me and seeUser

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import logger from "morgan";
 import schema from "./schema";
 import "./passport";
 import { athenticateJwt } from "./passport";
-import { prisma } from "../generated/prisma-client";
+import { isAuthenticated } from "./middleware";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request })
+  context: ({ request }) => ({ request, isAuthenticated })
 });
 
 server.express.use(logger("dev"));
